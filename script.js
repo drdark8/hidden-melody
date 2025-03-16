d71e36e3df1309f642ef132be3cd4e985fe47864
drdark8 <danyelm1023@gmail.com>
2025-03-16 20:03:58 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-<audio id="myAudio" loop>
-    <source src="https://dl.my-ahangha.ir/up/2021/Amir%20Tataloo%20-%20Man%20Bahat%20Ghahram.mp3" type="audio/mpeg">
-</audio>
+window.onload = function() {
+    document.getElementById("myAudio").play().catch(error => {
+        console.log("مرورگر اجازه پخش خودکار نمیدهد، روی دکمه کلیک کنید.");
+    });
+};
 
-<script>
-    window.onload = function() {
-        document.getElementById("myAudio").play();
-    };
-</script>
+function playMusic() {
+    document.getElementById("myAudio").play();
+}
